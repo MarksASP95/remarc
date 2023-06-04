f2e0337d1619da2fefceade443f9cd40da83672a
Marco <marcosuarezp95@gmail.com>
2023-06-04 14:56:01 -0400

    imp spinner

---
+++ src/app/components/RemarcSpinner/RemarcSpinner.component.tsx
@@ -0,0 +1,11 @@
+import "./RemarcSpinner.styles.scss";
+
+export default function RemarcSpinner() {
+  return (
+    <div className="remarc-spinner-container">
+      <div className="loadingio-spinner-spinner-sycg6umvy4r"><div className="ldio-gm66r4kzq9q">
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      </div></div>
+    </div>
+  )
+}
