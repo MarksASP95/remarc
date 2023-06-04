5d463cc1f0694367a8f3fa944c2ce5abee0e7c4a
Marco <marcosuarezp95@gmail.com>
2023-06-04 14:55:37 -0400

    imp auth utilities

--- src/app/hooks/auth.hooks.ts
+++ src/app/hooks/auth.hooks.ts
@@ -1,10 +1,12 @@
 import { RemarcUser } from "@/models/user.models";
 import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
 import { getSupabaseClient } from '../utils/supabase';
 
 export const useAuth = () => {
   
   const supabase = getSupabaseClient();
+  const router = useRouter();
 
   const signIn = async (email: string, password: string): Promise<User | null> => {
     const result = await supabase.auth.signInWithPassword({
@@ -15,6 +17,29 @@ export const useAuth = () => {
     if (result.error) throw result.error;
 
     return result.data?.user || null;
+  }
+
+  const subscribeToAuthStateChanges = () => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_OUT') {
+        // delete cookies on sign out
+        const expires = new Date(0).toUTCString()
+        document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`
+        document.cookie = `my-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`
+      } else if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
+        if (!session) return;
+        const maxAge = 100 * 365 * 24 * 60 * 60 // 100 years, never expires
+        document.cookie = `my-access-token=${session.access_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`
+        document.cookie = `my-refresh-token=${session.refresh_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`
+      }
+    })
+  }
+
+  const signOut = () => {
+    return supabase.auth.signOut()
+      .then(() => {
+        router.push("/login");
+      });
   }
 
   const getAuthUser = async (): Promise<User | null> => {
@@ -51,7 +76,9 @@ export const useAuth = () => {
 
   return {
     signIn,
+    signOut,
     getAuthUser,
     getCurrentRemarcUser,
+    subscribeToAuthStateChanges,
   }
 }
